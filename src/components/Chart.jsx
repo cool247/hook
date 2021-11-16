a1faee153c9b581d92c58420c841ea6233b90054
sadanand <sadanandgpl@gmail.com>
2021-11-16 22:40:15 +0530

    merger conflict resolved

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -8,6 +8,7 @@ const data = {
     {
       label: '# of Votes',
       data: [12, 19, 3, 5, 2, 3],
+
       backgroundColor: [
         'rgba(255, 99, 132, 0.2)',
         'rgba(54, 162, 235, 0.2)',
