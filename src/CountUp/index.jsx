313880b91534dff4a3a807a9d9080703c5fb38c6
sadanand <sadanandgpl@gmail.com>
2021-12-13 01:07:50 +0530

    count uP with custom Hook

--- src/CountUp/index.jsx
+++ src/CountUp/index.jsx
@@ -2,23 +2,33 @@ import React, { useState, useEffect, useRef } from 'react';
 import { Button, Typography } from '@material-ui/core';
 import CountUp from 'react-countup';
 
+function usePrevious(value) {
+  const ref = useRef(0);
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
 const Count = () => {
   const [count, setCount] = useState(0);
-  const prevCountRef = useRef();
-
-  useEffect(() => {
-    prevCountRef.current = count;
-  });
-
-  const prevCount = prevCountRef.current;
+  const prevCount = usePrevious(count);
 
   return (
     <>
       <Typography align="center">
         <CountUp start={prevCount} end={count} duration={0.75} />
       </Typography>
-      <Button variant="outlined" color="primary" onClick={() => setCount((ps) => ps + 10)}>
+      <Button
+        style={{ marginRight: '8px' }}
+        variant="outlined"
+        color="primary"
+        onClick={() => setCount((ps) => ps + 10)}
+      >
         Count UP
+      </Button>
+      <Button variant="outlined" color="primary" onClick={() => setCount((ps) => ps - 10)}>
+        Count DOWN
       </Button>
     </>
   );
