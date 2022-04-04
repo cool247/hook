98a2f075987cee48645b31b54e2675c7a53af07d
sadanand <sadanandgpl@gmail.com>
2022-04-04 22:56:20 +0530

    clsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 //import FetchAPI from "./fetchAPI";
-import React, { Suspense } from "react";
+import React from "react";
 import "./App.css";
-import { CircularProgress } from "@material-ui/core";
+import clsx from "clsx";
 // import ComponentA from "./componentA";
 // import ComponentC from "./componentC";
 // import ComponentB from "./componentB";
@@ -12,52 +12,42 @@ import { CircularProgress } from "@material-ui/core";
 // import Todo from "../app";
 import Input from "./atom";
 import { ErrorBoundary } from "react-error-boundary";
-import { Fallback } from "./Fallback";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { theme, darkTheme } from "./theme";
 import { MuiButton } from "../ComponentLibrary/index";
+import { makeStyles } from "@material-ui/core/styles";
 
-// import Home from "./Home";
-// import Testing from "../routing/buttons";
 import { Switch, Route, Link } from "react-router-dom";
-// import Test from "../react-hook-form";
-// import AutoCompleteVirtualize from '../AutoCompleteVirtualize';
-// import Menu from './Menu';
-// import Chart from './Chart';
-// import DatePicker from '../DatePicker';
-// export const nameContext = React.createContext();
-// export const channelContext = React.createContext();
-// export const CountContext = React.createContext();
-// const initialState = { count: 0 };
-// const reducer = (state, action) => {
-//   switch (action.type) {
-//     case "increment":
-//       return { count: state.count + 1 };
-//     case "decrement":
-//       return { count: state.count - 1 };
-//     case "reset":
-//       return { count: 0 };
-
-//     default:
-//       return state;
-//   }
-// };
-// import Count from '../CountUp';
 
 const Home = React.lazy(() => import("../routing/Home"));
 const About = React.lazy(() => import("../routing/About"));
 const User = React.lazy(() => import("../routing/User"));
 
+// import Home from "../routing/Home";
+// import About from "../routing/About";
+// import User from "../routing/User";
+
+const useStyles = makeStyles({
+  root: {
+    width: 100,
+  },
+  active: {
+    backgroundColor: "red",
+  },
+  notActive: {
+    backgroundColor: "green",
+  },
+});
+
 function App() {
+  const classes = useStyles();
   const [isDarkTheme, setDarkTheme] = React.useState(false);
 
   const errorHandler = (error, errorInfo) => {
-    console.log("==============A-HMIS(START)=================");
+    console.log("==============A-(START)=================");
     console.log(error, errorInfo);
-    console.log("==============A-HMIS(END)====================");
+    console.log("==============A-(END)====================");
   };
-
-  console.log(isDarkTheme);
 
   const handleModeButtonClick = () => {
     setDarkTheme(ps => !ps);
@@ -68,19 +58,7 @@ function App() {
       <ThemeProvider theme={isDarkTheme ? darkTheme : theme}>
         <div>
           <MuiButton onClick={handleModeButtonClick}>Change theme</MuiButton>
-          {/* <Input /> */}
-
-          {/* <ChatExample /> */}
-          {/* <Test /> */}
-          {/* <Count /> */}
           <Input />
-          {/* <Todo /> */}
-          {/* <Test id="abc" /> */}
-          {/* <AutoCompleteVirtualize /> */}
-          {/* <Chart /> */}
-          {/* <DatePicker /> */}
-          {/* <Mtable /> */}
-          {/* <Testing /> */}
           <div>
             <ul>
               <li>
@@ -94,7 +72,15 @@ function App() {
               </li>
             </ul>
           </div>
-
+          <button
+            className={clsx({
+              [classes.root]: true,
+              [classes.active]: isDarkTheme,
+              [classes.notActive]: !isDarkTheme,
+            })}
+          >
+            HI
+          </button>
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/user" component={User} />
