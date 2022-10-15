aff5837a65ab309b4e1b978731a6ac4f563b5742
Sadanand Gopal <sadanandgpl@gmail.com>
2022-10-15 20:56:55 +0530

    2nd  approach

--- src/test.js
+++ src/test.js
@@ -50,3 +50,27 @@ const recursive_lists = (data) => {
   };
 
   console.log(recursive_lists(entries))
+
+
+  function list_to_tree(list) {
+    var map = {}, node, roots = [], i;
+    
+    for (i = 0; i < list.length; i += 1) {
+      map[list[i].id] = i; // initialize the map
+      list[i].children = []; // initialize the children
+    }
+    
+    for (i = 0; i < list.length; i += 1) {
+      node = list[i];
+      if (node.parentId !== "0") {
+        // if you have dangling branches check that map[node.parentId] exists
+        list[map[node.parentId]].children.push(node);
+      } else {
+        roots.push(node);
+      }
+    }
+    return roots;
+  }
+
+  
+  console.log(list_to_tree(entries))
